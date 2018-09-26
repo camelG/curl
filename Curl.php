3a7add8988a4d1c4ed83c119715ed2b4f0d2682a
Tani <tani@next-investment.co.jp>
2018-09-26 16:59:58 +0900

    first commit

---
+++ Curl.php
@@ -0,0 +1,29 @@
+<?php
+class Curl{
+    public function curlData($url, $data,$data_type = 'json', $type = 'POST')
+    {
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $type);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: multipart/form-data'));
+
+        if(! $result = curl_exec($ch) ){
+           echo curl_error($ch);
+        }
+        curl_close($ch);
+
+        if($data_type == 'json'){
+            $res = json_decode($result);
+        }
+
+        if($data_type == 'string'){
+            parse_str($result, $res); 
+        }
+
+        return $res;
+    }
+
+}
